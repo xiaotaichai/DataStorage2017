3135902f636504a5bd212e7afdaed7664e933ec4
lydialaseur <lydia.laseur@ncf.edu>
2017-11-02 23:54:49 -0400

    modified queries and views to check if input is out of bounds. added estimated fees to results.

--- app/queries.py
+++ app/queries.py
@@ -1,17 +1,33 @@
 import sqlalchemy
 
 def get_engine():
-    return sqlalchemy.create_engine("sqlite:///app/sample_trips.db")
+    return sqlalchemy.create_engine("postgresql://Lydia@localhost:5432/taxi")
+
+_get_lat_lng_bounds = """
+SELECT LEAST(MIN(pick_lat),MIN(drop_lat)) AS min_lat,
+       GREATEST(MAX(pick_lat),MAX(drop_lat)) AS max_lat,
+       LEAST(MIN(pick_lng),MIN(drop_lng)) AS min_lng,
+       GREATEST(MAX(pick_lng),MAX(drop_lng)) AS max_lng
+       FROM std_fare
+"""
+
+def get_bounds():
+    sql_engine = get_engine()
+    sql_connection = sql_engine.connect()
+    result = sql_connection.execute(_get_lat_lng_bounds)
+    lat_min, lat_max, long_min, long_max = result.fetchone()
+    result.close()
+    return(lat_min, lat_max, long_min, long_max)
 
 _comparables_query = """
-WITH closest_trips (fare_amount, tip_amt) AS (
-    SELECT fare_amount, tip_amt
-    FROM trips
-    ORDER BY abs(pickup_latitude-({pickup_latitude})) + abs(pickup_longitude-({pickup_longitude})) +
-        abs(dropoff_latitude-({dropoff_latitude})) + abs(dropoff_longitude-({dropoff_longitude}))
+WITH closest_trips (fare_amt, tip_amt, fees_amt) AS (
+    SELECT fare, tip, tolls_fees
+    FROM std_fare
+    ORDER BY abs(pick_lat-({pickup_latitude})) + abs(pick_lng-({pickup_longitude})) +
+        abs(drop_lat-({dropoff_latitude})) + abs(drop_lng-({dropoff_longitude}))
     LIMIT 20
 )
-SELECT AVG(fare_amount) AS fare_amount, AVG(tip_amt) AS tip_amt
+SELECT AVG(fare_amt) AS fare_amt, AVG(tip_amt) AS tip_amt, AVG(fees_amt) AS fees_amt
 FROM closest_trips
 """
 
@@ -29,4 +45,3 @@ if __name__ == '__main__':
     result = conn.execute(generate_comparables_query(from_lat, from_long, to_lat, to_long))
     print(result.fetchone())
     result.close()
-
--- app/views.py
+++ app/views.py
@@ -14,6 +14,7 @@ from app import app, queries
 from secrets import maps_api_key
 
 sql_engine = queries.get_engine()
+lat_min, lat_max, long_min, long_max = queries.get_bounds()
 
 create_render_kw = lambda coordinate: {"placeholder": coordinate, "class": "form-control", "type": "number", "step": "any"}
 
@@ -22,6 +23,7 @@ class LocationForm(FlaskForm):
     from_long = DecimalField('Origin Longitude', places=4, validators=[DataRequired()], render_kw=create_render_kw("Longitude"))
     to_lat = DecimalField('Destination Latitude', places=4, validators=[DataRequired()], render_kw=create_render_kw("Latitude"))
     to_long = DecimalField('Destination Longitude', places=4, validators=[DataRequired()], render_kw=create_render_kw("Longitude"))
+
 
 @app.route('/')
 @app.route('/index')
@@ -35,23 +37,36 @@ def results():
     form = LocationForm(csrf=False)
     if not form.validate_on_submit():
         for field in form:
-          if not field.validate(form):
-            flash("Invalid value for {name}".format(name=field.label.text))
-        return redirect('/')
+            if not field.validate(form):
+                flash("Invalid value for {name}".format(name=field.label.text))
+                return redirect('/')
+
+    for field in form:
+        if field.label.text in ["Origin Latitude","Destination Latitude"] and not (lat_min <= field.data <= lat_max):
+            flash("Value for {name} out of bounds.".format(name=field.label.text))
+            flash("Latitudes may range from {lat_min} to {lat_max}".format(lat_min=lat_min,lat_max=lat_max))
+            return redirect('/')
+        elif (field.label.text in ["Origin Longitude","Destination Longitude"]) and not (long_min <= field.data <= long_max):
+            flash("Value for {name} out of bounds.".format(name=field.label.text))
+            flash("Longitudes may range from {long_min} to {long_max}".format(long_min=lat_min,long_max=lat_max))
+            return redirect('/')
     sql_connection = sql_engine.connect()
-    result = sql_connection.execute(queries.generate_comparables_query(form.from_lat.data, 
-                                                                       form.from_long.data, 
-                                                                       form.to_lat.data, 
+    result = sql_connection.execute(queries.generate_comparables_query(form.from_lat.data,
+                                                                       form.from_long.data,
+                                                                       form.to_lat.data,
                                                                        form.to_long.data))
-    estimated_fare, estimated_tip = result.fetchone()
+
+    estimated_fare, estimated_tip, estimated_fees = result.fetchone()
     result.close()
+
     return render_template('results.html',
-                           title='Results', 
-                           estimated_fare=estimated_fare, 
-                           estimated_tip=estimated_tip, 
+                           title='Results',
+                           estimated_fare=estimated_fare,
+                           estimated_tip=estimated_tip,
+                           estimated_fees=estimated_fees,
                            origin="{0},{1}".format(form.from_lat.data, form.from_long.data),
                            destination="{0},{1}".format(form.to_lat.data, form.to_long.data),
-                           maps_api_key=maps_api_key)    
+                           maps_api_key=maps_api_key)
 
 # remove this when we deploy on apache, and route the static content separately
 @app.route('/static/<path:path>')
